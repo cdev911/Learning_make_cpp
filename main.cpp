72e09c5b366173c773b1a0164b65aa261a512fc5
cdev911 <cdev911@gmail.com>
2017-10-06 10:09:12 +0200

    Point and Recatange created, and the area is displayed

--- main.cpp
+++ main.cpp
@@ -1,8 +1,17 @@
 #include <iostream>
 
+#include "Point.h"
+#include "Rectangle.h"
+
 int main(int argc, char const *argv[]) {
 
   std::cout<<"Hello world"<<std::endl;
 
+  Point bottom_left = Point( 5, 2);
+  Point top_right = Point(7,4);
+
+  Rectangle rec = Rectangle(bottom_left, top_right);
+
+  std::cout << "area of the Rectangle is " << rec.area() << '\n';
   return 0;
 }
